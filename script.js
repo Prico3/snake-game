3744f71ada270287d5a580674ac5d36554b37cee
Prico3 <pricoco.andrea@gmail.com>
2023-05-31 17:28:51 +0200

    Add: game over conditions

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ let cols = 20;
 let board;
 let context;
 
-//snake head
+//snake 
 let snakeX = blockSize * 5;
 let snakeY = blockSize * 5;
 
 let velocityX = 0;
 let velocityY = 0;
 
+let snakeBody = [];
+
 //food
 let foodX;
 let foodY;
+
+//gameover
+let gameOver = false;
 
 
 //create the board
@@ -32,6 +37,9 @@ window.onload = function () {
 }
 
 function update() {
+    if (gameOver) {
+        return;
+    }
     //board
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
@@ -40,9 +48,17 @@ function update() {
     context.fillStyle = "red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
 
-    //when snake eat food, food spawn in another location
+    //when snake eat food, food spawn in another location, and snake grow
     if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY])
         placeFood();
+    }
+
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY]
     }
 
     //snake
@@ -50,11 +66,28 @@ function update() {
     snakeX += velocityX * blockSize;
     snakeY += velocityY * blockSize;
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    //gameover conditions
+    if (snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > rows * blockSize) {
+        gameOver = true;
+        alert("Game Over");
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
 
 }
 
+//Change directions of the snake 
 function changeDirection(e) {
-    if (e.code == "ArrowUp" && velocityY != 1) {
+    if (e.code == "ArrowUp" && velocityY != 1) { //This cannot alloud to, if the snake go up, go down
         velocityX = 0;
         velocityY = -1;
     }
