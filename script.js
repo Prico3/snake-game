e2f59536b22b6061fd71a15d655d3bf491486b5f
Prico3 <pricoco.andrea@gmail.com>
2023-05-31 15:04:51 +0200

    Add: snake and food(spawn random)

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ let cols = 20;
 let board;
 let context;
 
+//snake head
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+//food
+let foodX;
+let foodY;
+
 
 //create the board
 window.onload = function () {
@@ -19,6 +27,21 @@ window.onload = function () {
 }
 
 function update() {
-    context.fillstyle = "black";
+    //board
+    context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
+
+    //snake
+    context.fillStyle = "lime";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    //food
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+//place the food randomly
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
 }
