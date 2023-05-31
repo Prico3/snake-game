35713751610aa09f2323183f3433836d859017e6
Prico3 <pricoco.andrea@gmail.com>
2023-05-31 16:46:31 +0200

    Add: snake movement

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let context;
 let snakeX = blockSize * 5;
 let snakeY = blockSize * 5;
 
+let velocityX = 0;
+let velocityY = 0;
+
 //food
 let foodX;
 let foodY;
@@ -21,8 +24,10 @@ window.onload = function () {
     board.width = cols * blockSize; //set the width
     context = board.getContext("2d"); //used for drawing on the board
 
-
-    update();
+    placeFood();
+    document, addEventListener("keyup", changeDirection);
+    //update();
+    setInterval(update, 1000 / 10)
 
 }
 
@@ -31,13 +36,35 @@ function update() {
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
 
-    //snake
-    context.fillStyle = "lime";
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
     //food
     context.fillStyle = "red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    //snake
+    context.fillStyle = "lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
 }
 
 //place the food randomly
